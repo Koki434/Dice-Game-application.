a266332136c7df5bf53673fcf06373654a43e4fa
Koki434 <s1310209@u-aizu.ac.jp>
2025-07-03 16:12:11 +0900

    Update dice_game.c

--- dice_game.c
+++ dice_game.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+    int die1 = rand() % 6 + 1;
+    int die2 = rand() % 6 + 1;
+    int total = die1 + die2;
+
+    printf("Rolling dice...\n");
+    printf("Die 1: %d\n", die1);
+    printf("Die 2: %d\n", die2);
+    printf("Total value: %d\n", total);
+
+    if (total > 7) {
+        printf("You won!\n");
+    } else {
+        printf("You lost!\n");
+    }
+
+    return 0;
+}
+
 
